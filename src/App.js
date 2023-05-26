8ab2a35af1e64f2202ab31c8c686e7e80342760e
hebiscus <107350293+hebiscus@users.noreply.github.com>
2023-05-26 19:38:28 +0200

    rendering empty cards

--- src/App.js
+++ src/App.js
@@ -1,8 +1,42 @@
 import './App.css';
+import { useEffect, useState } from 'react';
+import Card from './components/Card';
 
 function App() {
+
+  const initialCards = [
+    {src: "", id: 0},
+    {src: "", id: 1},
+    {src: "", id: 2},
+    {src: "", id: 3},
+    {src: "", id: 4},
+    {src: "", id: 5},
+    {src: "", id: 6},
+    {src: "", id: 7},
+    {src: "", id: 8},
+  ];
+
+  const [cards, setCards] = useState(initialCards)
+
+  useEffect(() => {
+    function shuffleArray(array) {
+      for (let i = array.length - 1; i > 0; i--) {
+          const j = Math.floor(Math.random() * (i + 1));
+          [array[i], array[j]] = [array[j], array[i]];
+      }
+
+      // const shuffledCards = shuffleArray(cards);
+      // setCards(shuffledCards)
+    }
+
+    shuffleArray(initialCards)
+  }, [cards])
+
   return (
     <div>
+      {cards.map(card => {
+        return <Card />
+      })}
     </div>
   );
 }
---
+++ src/components/Card.js
@@ -0,0 +1,12 @@
+import { useState } from 'react';
+
+function Card(props) {
+
+    return (
+        <button>
+            <img></img>
+        </button>
+    )
+}
+
+export default Card;
