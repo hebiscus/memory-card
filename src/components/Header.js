ce148197685efe42c7b395ef3f32922ba03e7e11
hebiscus <107350293+hebiscus@users.noreply.github.com>
2023-05-28 17:35:49 +0200

    add header subtext + it's styling

--- src/components/Header.js
+++ src/components/Header.js
@@ -5,8 +5,11 @@ function Header(props) {
 
     return (
         <div className="header">
+        <div className="points">
             <h4>Current Score: {props.currentScore}</h4>
             <h4>Best Score: {props.bestScore}</h4>
+        </div>
+            <h5>Don't click on the same image twice!</h5>
         </div>
     )
 }
